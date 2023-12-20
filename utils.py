54d9599d8e4ab2c849fc9a1f69b3460f055490f4
Ron Ferens <ronferens@users.noreply.github.com>
2023-12-20 09:54:34 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,25 @@
 from typing import List, Dict, Union
 import torch
 import json
+import numpy as np
 
 
 def set_inputs(prompt: Union[str, List[str]], batch_size: int = 1, nsteps: int = 20, device: str = 'cuda',
-               seed: int = None, seed_offset: int = 0) -> Dict:
+               seed: int = None) -> Dict:
     # Incase a single prompt was given - setting the number of outputs according to the requested batch size
     if isinstance(prompt, str):
         prompt = batch_size * [prompt]
 
+    if seed is None:
+        # Applying random seed to each prompt
+        seeds_list = np.random.randint(batch_size * 10, size=batch_size)
+    elif isinstance(seed, int):
+        # Applying the same seed for all prompts
+        seeds_list = batch_size * [seed]
+
     generator_list = []
-    seeds_list = []
     for idx in range(len(prompt)):
-        seeds_list.append(seed if seed is not None else (idx + seed_offset))
-        generator_list.append(torch.Generator(device).manual_seed(seeds_list[-1]))
+        generator_list.append(torch.Generator(device).manual_seed(int(seeds_list[idx])))
     num_inference_steps = nsteps
 
     return {'prompt': prompt, 'generator': generator_list, 'num_inference_steps': num_inference_steps}, seeds_list
@@ -22,7 +28,7 @@ def set_inputs(prompt: Union[str, List[str]], batch_size: int = 1, nsteps: int =
 def save_generation_metadata(inputs: Dict, seeds: List, out_filename: str) -> None:
     # Setting the parameters to save
     inputs.pop('generator')
-    inputs['seeds'] = seeds
+    inputs['seeds'] = seeds.tolist()
     inputs['num_inference_steps'] = len(inputs['seeds']) * [inputs['num_inference_steps']]
 
     # Serializing json
